9f9872f4824b3686cdefc85c38058378f5c534ae
Legofan0807 <83748124+Legofan0807@users.noreply.github.com>
2022-09-10 12:22:13 +0200

    Changed the  Project's name to "bombersnek"

--- Code/Engine.h
+++ Code/Engine.h
@@ -3,5 +3,5 @@
 #define IS_IN_EDITOR true
 #define VERSION_STRING "0.1"
 #define ENGINE_DEBUG true
-constexpr char ProjectName[] = "BomberJam64";
+constexpr char ProjectName[] = "BomberSnek";
 constexpr char StartupMap[] = "TestScene";
